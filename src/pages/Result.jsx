e34cb6a86adbb9c9abad9b17126a7c18d04f61ab
830Utpal <lakshman2016yadav@gmail.com>
2025-03-10 13:04:39 +0530

    result page

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const Result = () => {
   return (
+    <form className='flex flex-col min-h-[90vh] justify-center items-center'>
     <div>
-      Result page
+    <div className='relative'>
+      <img src={assers.sample_img_1} alt="" className='max-w-sm rounded'/>
+      <span className='absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]'/>
     </div>
+    <p>Loading.....</p>
+    </div>
+    <div className='flex w-full max-w-xl bg-neautral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
+      <input type="text" placeholder='Describe what you want to generate.'className='flex-1 bg=transparent outline-none ml-8 max-sm:w-20'/>
+      <button type="submit"
+      className='bg-zinc-900 px-10 sm:px-16 py-3 rounded-full'>Generate</button>
+    </div>
+  </form>
   )
 }
 
