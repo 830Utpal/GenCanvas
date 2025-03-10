9934b398d90c1055b594bd25b6be5aeea05268a9
830Utpal <lakshman2016yadav@gmail.com>
2025-03-10 15:22:16 +0530

    frontend signup cross_btn

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,17 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
 
 const Login = () => {
     const[state,setState]=useState('Login')
+    const {setShowLogin}=useContext(AppContext)
+
+    useEffect(()=>{
+         document.body.style.overflow='hidden'
+            return ()=>{
+                document.body.style.overflow='unset'
+            }
+    },[])
   return (
     <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
       <form className='relative bg-white p-10 rounded-xl text-slate-500'>
@@ -23,11 +32,11 @@ const Login = () => {
         <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot password?</p>
         <button className='bg-blue-600 w-full text-white py-2 rounded-full'>{state === 'Login' ? 'login' : 'Create account'}</button>
 
-        { state==='Login' ?<p className='mt-5 text-center'>Don't have an account? <span className='text-blue-600'>Sign up</span></p>
+        { state==='Login' ?<p className='mt-5 text-center'>Don't have an account? <span className='text-blue-600 cursor-pointer' onClick={()=>setState('Sign Up')}>Sign Up</span></p>
          :
-         <p className='mt-5 text-center'>    Already have an account? <span className='text-blue-600'>Login</span></p>}
+         <p className='mt-5 text-center'>Already have an account? <span className='text-blue-600 cursor-pointer' onClick={()=>setState('Login')}>Login</span></p>}
 
-        <img src={assets.cross_icon} alt="" className='absolute top-5 right-5 cursor-pointer'/>
+        <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" className='absolute top-5 right-5 cursor-pointer'/>
       </form>
     </div>
   )
